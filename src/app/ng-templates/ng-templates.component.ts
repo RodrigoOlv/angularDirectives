4e85f0c66bb346496bd0186338a9a0b84fce85f2
RodrigoOlv <rodrigo.oliveira1998@outlook.com>
2019-04-25 14:41:16 -0300

    primeiras experiencias com diretivas

---
+++ src/app/ng-templates/ng-templates.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-templates',
+  templateUrl: './ng-templates.component.html',
+  styleUrls: ['./ng-templates.component.css']
+})
+export class NgTemplatesComponent implements OnInit {
+
+  enable = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
